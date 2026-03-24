b066ee20b9ded932c0f591b70510249b92b8ae4b
josh <101273431+joshVale@users.noreply.github.com>
2026-03-24 13:59:59 +0600

    Time: 0 ms (100%), Space: 12.5 MB (26.37%) - LeetHub

--- 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.cpp
+++ 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.cpp
@@ -4,13 +4,7 @@ public:
         int max = *max_element(candies.begin(),candies.end());
         vector<bool>ans;
         for(int i=0;i<candies.size();i++){
-            candies[i]=candies[i]+extraCandies;
-            if(candies[i]>=max){
-                ans.push_back(true);
-            }
-            else{
-                ans.push_back(false);
-            }
+            ans.push_back(max<=(candies[i]+extraCandies));
         }
         return ans;
     }
